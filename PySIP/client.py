0a1f1096e35739cd0c8b5e5176c7245e6f69299e
Moha Abdi <83307623+moha-abdi@users.noreply.github.com>
2024-01-06 22:14:50 +0300

    version update to -> 1.3.0

--- PySIP/client.py
+++ PySIP/client.py
@@ -338,7 +338,7 @@ class Client:
             msg += f"Min-SE: 90\r\n"
             # msg += f"Client-Checksum: {generated_checksum.checksum}\r\n"
             msg += 'Location:{"MNC":"01","MCC":"637"}\r\n'
-            msg += f"User-Agent: PySIP-1.2.0\r\n"
+            msg += f"User-Agent: PySIP-1.3.0\r\n"
             # msg += f"Client-Timestamp: {generated_checksum.timestamp}\r\n"
             msg += f"Content-Type: application/sdp\r\n"
 
