ae99d901d3dd444fe2c81725409689b7df1e1022
moha-abdi <mohaa6052@gmail.com>
2024-03-27 14:52:32 +0300

    Add mp3_to_wav util

--- PySIP/utils/__init__.py
+++ PySIP/utils/__init__.py
@@ -1,3 +1,20 @@
+import asyncio
+import io
+from edge_tts import Communicate
+from pydub.audio_segment import AudioSegment
+
+
+def mp3_to_wav(f, target_channels = 1, target_framerate = 8000):
+    """
+    Convert MP3 audio from the temporary chunk to WAV format.
+    """
+    output_chunk = io.BytesIO()
+    decoded_chunk = AudioSegment.from_mp3(f)
+    decoded_chunk = decoded_chunk.set_channels(target_channels)
+    decoded_chunk = decoded_chunk.set_frame_rate(target_framerate)
+    decoded_chunk.export(output_chunk, format='wav')
+    return output_chunk
+
 async def generate_audio(text: str, voice: str) -> io.BytesIO:
     """
     this genertes the real TTS using edge_tts for this part.
